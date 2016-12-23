c1e2523c272d48d2d6a3b754b3ef4f7f4d302df9
Elana Kopelevich <ekopelevich@gmail.com>
2016-12-23 12:05:29 -0700

    undo env var changes from last commit and comment out secret reference in prd.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,12 +9,12 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
 config :mmserver, Mmserver.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: "Mt0SQF7zq4lTyy155vdLwBbIk5eTpOCRW8bm6XRQiMARp83mS/+Mbzmuc2blnyJh"
 
 # Configure your database
 config :mmserver, Mmserver.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATABASE_USERNAME")
-  password: System.get_env("DATABASE_PASSWORD"),
+  username: "postgres",
+  password: "postgres",
   database: "mmserver_prod",
   pool_size: 20
